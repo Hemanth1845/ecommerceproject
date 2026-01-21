fe381ff16abec8b66d2c07b32a9d5fc02ac2ce4e
Hemanth <2300032944@kluniversity.in>
2026-01-21 22:48:32 +0530

    fullstackproject

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,16 @@
-const express = require("express");
 const mysql = require("mysql2");
-const app = express();
-app.use(express.json());
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "rahul",
+  user: "ecom_user",
+  password: "ecom123",
   database: "ecommerce",
   port: 3306
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Error occurred:", err);
+    console.log("Database connection failed:", err);
   } else {
     console.log("Database connected successfully");
   }
